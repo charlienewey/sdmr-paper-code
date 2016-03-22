39e8e3fd55ad6cbac44d35158d172e969db58b1b
Charles Newey <charlie@assemblyco.de>
2016-03-22 22:32:31 +0000

    Compare two images

--- gaussian.py
+++ gaussian.py
@@ -11,6 +11,11 @@ io.use_plugin("matplotlib")
 
 from sklearn.cluster import MiniBatchKMeans as k_means
 
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import homogeneity_score
+from sklearn.metrics import completeness_score
+
 from skimage.feature import local_binary_pattern
 from skimage.feature import blob_dog
 
@@ -44,45 +49,111 @@ def neighbourhoodify(image, nbhd=10):
     return avgs
 
 
-def _imshow(image):
-    plt.figure()
-    io.imshow(image)
+def _imshow(*images):
+    fig = plt.figure()
+    for i in range(0, len(images)):
+        im = images[i]["image"]
+        if im.shape[0] > im.shape[1]:
+            ax = fig.add_subplot(1, len(images), i + 1)
+        else:
+            ax = fig.add_subplot(len(images), 1, i + 1)
+
+        if "cmap" in images[i]:
+            cmap = images[i]["cmap"]
+        else: cmap = plt.cm.inferno
+
+        cax = ax.imshow(images[i]["image"], cmap=cmap)
+        fig.colorbar(cax)
 
 
-if __name__ == "__main__":
-    # set up input
-    path = os.path.abspath(os.path.join(os.getcwd(), sys.argv[1]))
+def segment(path, n_clusters=5):
     image = io.imread(path, as_grey=True)
 
     # downscale and smooth
-    blur_image = gaussian_filter(image, 15)  # cv2.pyrUp(cv2.pyrDown(image))
+    blur_image = gaussian_filter(image, 15)
 
     # get texture features
-    num_feats, feats = lbp(blur_image, n=6, method="uniform")
+    num_feats, feats = lbp(blur_image, n=7, method="nri_uniform")
     feats_r = feats.reshape(-1, num_feats)
 
     # set up batch k-means
-    n_classes = 10
-    mbkm = k_means(n_classes)
+    km = k_means(n_clusters=n_clusters)
 
     # cluster the local binary patterns with default settings (k = 8)
-    clus = mbkm.fit(feats_r)
+    clus = km.fit(feats_r)
     labels = clus.labels_
 
     # reshape label arrays
     labels = labels.reshape(blur_image.shape[0], blur_image.shape[1])
 
+    return (image, blur_image, labels)
+
+def mask_dominant_class(image, labels):
     # make mask of dominant texture
-    hist, bins = np.histogram(labels, n_classes, [0, n_classes])
+    hist, bins = np.histogram(labels, n_clusters, [0, n_clusters])
     dominant_texture = bins[hist.argmax()]
 
     print("Dominant texture class: %d" % (dominant_texture))
     mask = labels == dominant_texture
+    image[mask] = 0
+
+    return mask, image
+
+
+def cluster_metrics(labels_1, labels_2):
+    print("\n".join(
+        [
+            "Normalized Mutual Information: %f" % (normalized_mutual_info_score(labels_1, labels_2)),
+            "Adjusted Rand Score: %f" % (adjusted_rand_score(labels_1, labels_2)),
+            "Homogeneity: %f" % (homogeneity_score(labels_1, labels_2)),
+            "Completeness: %f" % (completeness_score(labels_1, labels_2))
+        ]
+    ))
+
+
+if __name__ == "__main__":
+    # set up input
+    path = os.path.abspath(os.path.join(os.getcwd(), sys.argv[1]))
+    path_2 = os.path.abspath(os.path.join(os.getcwd(), sys.argv[2]))
+
+    # cluster images
+    n_clusters = 10
+    image, blur_image, labels = segment(path, n_clusters=n_clusters)
+    image_2, blur_image_2, labels_2 = segment(path_2, n_clusters=n_clusters)
+
+    # mask dominant textures
+    mask, blur_image = mask_dominant_class(blur_image, labels)
+    mask_2, blur_image_2 = mask_dominant_class(blur_image_2, labels_2)
 
     # plot histogram of intensity values across dominant texture
-    plt.hist(blur_image[np.where(mask)])
+    fig = plt.figure()
+
+    ax = fig.add_subplot(2, 1, 1)
+    ax.hist(image[np.where(mask)])
+
+    ax = fig.add_subplot(2, 1, 2)
+    ax.hist(image_2[np.where(mask_2)])
+
+    # get and print cluster metrics
+    print("Dominant cluster metrics: ")
+    cluster_metrics(mask.ravel(), mask_2.ravel())
+
+    print("\nOverall cluster metrics: ")
+    cluster_metrics(labels.ravel(), labels_2.ravel())
 
     # display images
-    _imshow(blur_image)
-    _imshow(labels)
+    _imshow({
+        "image": blur_image,
+        "cmap": plt.cm.gray
+    }, {
+        "image": blur_image_2,
+        "cmap": plt.cm.gray
+    })
+
+    _imshow({
+        "image": labels
+    }, {
+        "image": labels_2
+    })
+
     io.show()
