a647e46772b4a6c979c20c0dcdbc8313d89c09e4
Charles Newey <charlie@assemblyco.de>
2016-03-24 01:58:16 +0000

    Combine texture and colour segmentation

--- gaussian.py
+++ gaussian.py
@@ -1,13 +1,15 @@
 import itertools
 import os
 import sys
+import warnings
 
-import matplotlib.pyplot as plt
 import numpy as np
+
 import scipy.ndimage as ndi
 
+import matplotlib.pyplot as plt
+
 import skimage.io as io
-io.use_plugin("matplotlib")
 
 from sklearn.cluster import MiniBatchKMeans as k_means
 
@@ -19,10 +21,10 @@ from sklearn.metrics import completeness_score
 from skimage.exposure import equalize_hist
 
 from skimage.feature import local_binary_pattern
-from skimage.feature import blob_dog
 
 from skimage.filters import gabor_filter
 from skimage.filters import gaussian_filter
+from skimage.filters import threshold_otsu
 
 from skimage.util import view_as_blocks
 
@@ -69,10 +71,11 @@ def _imshow(*images):
 
 
 def segment(path, n_clusters=15):
-    image = equalize_hist(io.imread(path, as_grey=True))
+    image = io.imread(path, as_grey=True)
 
-    # downscale and smooth
-    blur_image = gaussian_filter(image, 10)
+    # blur and take local maxima
+    image = gaussian_filter(image, sigma=8)
+    blur_image = ndi.maximum_filter(image, size=3)
 
     # get texture features
     num_feats, feats = lbp(blur_image, n=5, method="uniform")
@@ -90,12 +93,13 @@ def segment(path, n_clusters=15):
 
     return (image, blur_image, labels)
 
-def mask_dominant_class(image, labels):
+
+def mask_dominant_label(image, labels):
     # make mask of dominant texture
     hist, bins = np.histogram(labels, n_clusters, [0, n_clusters])
     dominant_texture = bins[hist.argmax()]
 
-    print("Dominant texture class: %d" % (dominant_texture))
+    print("Dominant texture label: %d" % (dominant_texture))
     mask = labels == dominant_texture
     image[mask] = 0
 
@@ -113,6 +117,25 @@ def cluster_metrics(labels_1, labels_2):
     ))
 
 
+def segment_shadow(image, labels):
+    img = np.copy(image.ravel())
+    shadow_seg = np.copy(img)
+    for i in range(0, n_clusters):
+        # set up array of pixel indices matching cluster
+        mask = np.nonzero((labels.ravel() == i) == True)[0];
+        # if there are pixels with this label...
+        if len(mask) > 0:
+            if img[mask].var() > 0.01:
+                thresh = threshold_otsu(img[mask])
+                shadow = shadow_seg < thresh
+                shadow_seg[mask] = shadow[mask]
+            else:
+                shadow_seg[mask] = 0
+
+    shadow_seg = shadow_seg.reshape(*image.shape)
+    return shadow_seg
+
+
 if __name__ == "__main__":
     # set up input
     path = os.path.abspath(os.path.join(os.getcwd(), sys.argv[1]))
@@ -124,10 +147,14 @@ if __name__ == "__main__":
     image_2, blur_image_2, labels_2 = segment(path_2, n_clusters=n_clusters)
 
     # mask dominant textures
-    mask, blur_image = mask_dominant_class(blur_image, labels)
-    mask_2, blur_image_2 = mask_dominant_class(blur_image_2, labels_2)
+    mask, blur_image = mask_dominant_label(blur_image, labels)
+    mask_2, blur_image_2 = mask_dominant_label(blur_image_2, labels_2)
 
-    # plot histogram of intensity values across dominant texture
+    # segment shadows
+    shadow_seg = segment_shadow(image, labels)
+    shadow_seg_2 = segment_shadow(image_2, labels_2)
+
+    # plot histograms of intensity values across dominant texture
     fig = plt.figure()
 
     ax = fig.add_subplot(2, 1, 1)
@@ -143,19 +170,6 @@ if __name__ == "__main__":
     print("\nOverall cluster metrics: ")
     cluster_metrics(labels.ravel(), labels_2.ravel())
 
-    # display images
-    _imshow({
-        "image": blur_image,
-        "cmap": plt.cm.gray
-    }, {
-        "image": blur_image_2,
-        "cmap": plt.cm.gray
-    })
-
-    _imshow({
-        "image": labels
-    }, {
-        "image": labels_2
-    })
-
+    # show figures
+    _imshow({"image": shadow_seg}, {"image": shadow_seg_2})
     io.show()
